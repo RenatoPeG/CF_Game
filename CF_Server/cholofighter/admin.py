b7d2754c32c5be99efa31f89e88017af886bc4a5
christianempire <christianempire@hotmail.com>
2017-11-29 18:31:09 -0500

    Moving server

--- CF_Server/cholofighter/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import Character
-from . models import Scenario
-from . models import Score
-
-# Register your models here.
-admin.site.register(Character)
-admin.site.register(Scenario)
-admin.site.register(Score)
--- CF_Server/cholofighter/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CholofighterConfig(AppConfig):
-    name = 'cholofighter'
